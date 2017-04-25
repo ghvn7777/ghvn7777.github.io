86b5cd86b51038a875f1269ecf4b119f7254e166
Kaka_Ubuntu <hailongma61@gmail.com>
2017-04-26 00:58:32 +0800

    card

---
+++ locate.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+pelican content/
+cd output && python -m SimpleHTTPServer
---
+++ server.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+pelican content -s publishconf.py
+git checkout dev
+git add .
+git commit -m "$1"
+git push origin dev
+ghp-import output -b master
+git push origin master
